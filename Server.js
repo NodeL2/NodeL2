465e8a341089971911a08172db2464d45170487e
Dennis <dkoluris@outlook.com>
2021-05-23 10:49:18 +0300

    Coors

--- Server.js
+++ Server.js
@@ -1,57 +1,27 @@
 let net = require('net');
-let crypto = require('crypto');
 
 // User define
 let Config = require('./Config');
 let ServerSession = require('./ServerSession');
 let ServerMethods = require('./ServerMethods');
-let ClientMethods = require('./ClientMethods');
 
 class Server {
     constructor() {
         net.createServer(this.onSocket).listen(Config.loginServer.port, Config.loginServer.host, () => {
-            console.info('LS:: listening on %s:%s', Config.loginServer.host, Config.loginServer.port);
+            console.log('LS:: listening on %s:%s', Config.loginServer.host, Config.loginServer.port);
         });
     }
 
     onSocket(socket) {
-        console.info('LS:: incoming connection from %s:%s', socket.remoteAddress, socket.remotePort);
-
-        // Callback: Received data
-        socket.on('data', (data) => {
-            let packet = new Buffer.from(data, 'binary').slice(2).swap32();
-            let decipher = crypto.createDecipheriv('bf-ecb', Config.blowfishKey, '');
-            decipher.setAutoPadding(false);
-            packet = Buffer.concat([decipher.update(packet), decipher.final()]).swap32();
-
-            let opcode = packet[0];
-
-            switch (opcode) {
-                case 0x00:
-                    let hi = ClientMethods.authorizeLogin(packet);
-                    console.log(hi);
-                    break;
-
-                default:
-                    console.log('Unknown opcode -> 0x%s', Number(opcode).toString(16).padStart(2, '0'));
-                    break;
-            }
-        });
-
-        // Callback: Connection closed
-        socket.on('close', () => {
-            console.log('LS:: connection from %s:%s closed', socket.remoteAddress, socket.remotePort);
-        });
-
-        // Callback: Unknown error
-        socket.on('error', (error) => {
-            console.log('Connection %s error: %s', socket.remoteAddress, error.message);
-        });
-
+        console.log('LS:: incoming connection from %s:%s', socket.remoteAddress, socket.remotePort);
         socket.setEncoding('binary');
 
         let session = new ServerSession(socket);
-        session.sendPacket(ServerMethods.handshake(), false);
+        socket.on( 'data', session.receiveData.bind(session));
+        socket.on('close', session.connectionClosed.bind(session));
+        socket.on('error', session.connectionError.bind(session));
+
+        session.sendData(ServerMethods.handshake(), false);
     }
 }
 
--- ServerSession.js
+++ ServerSession.js
@@ -1,18 +1,52 @@
+let crypto = require('crypto');
+
+// User define
+let Config = require('./Config');
+let ClientMethods = require('./ClientMethods');
+
 class ServerSession {
     constructor(socket) {
         this.socket = socket;
     }
 
-    sendPacket(packet, encrypt = true) {
-        let header = new Buffer.from([packet.length + 2, 0x00]);
+    sendData(data, encrypt = true) {
+        let header = new Buffer.from([data.length + 2, 0x00]);
 
         if (encrypt) {
             console.log('Encryption failed');
         }
         else {
-            packet = Buffer.concat([header, packet]);
+            let packet = Buffer.concat([header, data]);
             this.socket.write(packet);
         }
+    }
+
+    receiveData(data) {
+        let packet = new Buffer.from(data, 'binary').slice(2).swap32();
+        let decipher = crypto.createDecipheriv('bf-ecb', Config.blowfishKey, '');
+        decipher.setAutoPadding(false);
+        packet = Buffer.concat([decipher.update(packet), decipher.final()]).swap32();
+
+        let opcode = packet[0];
+
+        switch (opcode) {
+            case 0x00:
+                let hi = ClientMethods.authorizeLogin(packet);
+                console.log(hi);
+                break;
+
+            default:
+                console.log('LS:: unknown opcode 0x%s', Number(opcode).toString(16).padStart(2, '0'));
+                break;
+        }
+    }
+
+    connectionClosed() {
+        console.log('LS:: connection from %s:%s closed', this.socket.remoteAddress, this.socket.remotePort);
+    }
+
+    connectionError(error) {
+        console.log('Connection %s error: %s', this.socket.remoteAddress, error.message);
     }
 }
 
