7e6e274e6ba2c5958436b979e5ab0bdd25aa1b89
Dennis Koluris <denniskoluris@rezolve.com>
2021-06-20 15:24:16 +0300

    Packets

--- src/Config.js
+++ src/Config.js
@@ -7,24 +7,29 @@ class Config {
     );
 
     static database = {
-        host     :  this.ini.Database.Hostname   ?? '127.0.0.1',
-        port     : +this.ini.Database.Port       ?? 3306,
-        user     :  this.ini.Database.User       ?? 'root',
-        password :  this.ini.Database.Password   ?? '',
-        db       :  this.ini.Database.Name       ?? 'nodel2'
+        host        :  this.ini.Database.Hostname   ?? '127.0.0.1',
+        port        : +this.ini.Database.Port       ?? 3306,
+        user        :  this.ini.Database.User       ?? 'root',
+        password    :  this.ini.Database.Password   ?? '',
+        db          :  this.ini.Database.Name       ?? 'nodel2'
     };
 
     static authServer = {
-        host     :  this.ini.AuthServer.Hostname ?? '127.0.0.1',
-        port     : +this.ini.AuthServer.Port     ?? 2106
+        host        :  this.ini.AuthServer.Hostname ?? '127.0.0.1',
+        port        : +this.ini.AuthServer.Port     ?? 2106
     };
 
     static gameServer = {
-        host     :  this.ini.GameServer.Hostname ?? '127.0.0.1',
-        port     : +this.ini.GameServer.Port     ?? 7777
+        host        :  this.ini.GameServer.Hostname ?? '127.0.0.1',
+        port        : +this.ini.GameServer.Port     ?? 7777
     };
 
-    static blowfishKey = '[;\'.]94-31==-%&@!^+]\u0000';
+    static client = {
+        chronicle   :  this.ini.Client.Chronicle    ?? 1,
+        blowfishKey :  this.ini.Client.BlowfishKey  ?? '[;\'.]94-31==-%&@!^+]\u0000'
+    }
+
+    static 
 }
 
 module.exports = Config;
