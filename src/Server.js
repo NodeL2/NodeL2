500942f5c7f424525380efea109e733366b8d0e9
Dennis <dkoluris@outlook.com>
2021-08-07 15:22:58 +0300

    Version

--- src/Server.js
+++ src/Server.js
@@ -12,6 +12,14 @@ let Database   = invoke('Database');
 let AuthServer = invoke('AuthServer/AuthServer');
 let GameServer = invoke('GameServer/GameServer');
 
+console.log('# ====================================');
+console.log('# Server Name: ...... NodeL2 [768]');
+console.log('# Build Revision: ... 0.01');
+console.log('# Chronicle: ........ C2');
+console.log('# Build date: ....... 2021.08.07 15:20');
+console.log('# NodeJS version: ... 14.17.x');
+console.log('# ====================================\n');
+
 Database.init(Config.database, () => {
     new AuthServer(Config.authServer);
     new GameServer(Config.gameServer);
