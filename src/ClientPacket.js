6f78cb9ecd10b44ad244b8a54a7e44061a2217a0
Dennis Koluris <denniskoluris@rezolve.com>
2021-08-23 16:43:46 +0300

    Rewrite

--- src/ClientPacket.js
+++ src/ClientPacket.js
@@ -1,50 +1,58 @@
 class ClientPacket {
     constructor(buffer) {
         this.buffer = buffer;
-        this.data   = [];
         this.offset = 1; // Skip the opcode id
+        this.data   = [];
+    }
+
+    append(value) {
+        this.data.push(value);
+    }
+
+    // Standard data types
+
+    read(size) {
+        switch (size) {
+            case 1: this.append(this.buffer.readUInt8    (this.offset)); break;
+            case 2: this.append(this.buffer.readUInt16LE (this.offset)); break;
+            case 4: this.append(this.buffer.readUInt32LE (this.offset)); break;
+        }
+
+        this.offset += size;
+        return this;
     }
 
     readC() {
-        this.data.push(
-            this.buffer.readUInt8(this.offset)
-        );
-        this.offset += 1;
+        return this.read(1);
+    }
 
-        return this;
+    readH() {
+        return this.read(2);
     }
 
     readD() {
-        this.data.push(
-            this.buffer.readInt32LE(this.offset)
-        );
-        this.offset += 4;
-
-        return this;
+        return this.read(4);
     }
 
+    // Special cases
+
     readB(size) {
-        this.data.push(
+        this.append(
             this.buffer.slice(this.offset, this.offset + size)
         );
-        this.offset += size;
 
+        this.offset += size;
         return this;
     }
 
     readS() {
-        let i;
-
-        for (i = this.offset; i < this.buffer.length; i += 2) {
-            if (this.buffer.readUInt16LE(i) === 0x00) {
+        for (let size = this.offset; size < this.buffer.length; size += 2) {
+            if (this.buffer.readUInt16LE(i) === 0x0000) {
+                this.append(this.buffer.toString('ucs2', this.offset, size));
+                this.offset += size + 1;
                 break;
             }
         }
-
-        this.data.push(
-            this.buffer.toString('ucs2', this.offset, i)
-        );
-        this.offset += i + 1;
 
         return this;
     }
