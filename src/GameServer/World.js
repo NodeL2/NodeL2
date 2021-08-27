2ec8b8d47043ea7eb1293551348ec8b444f301ab
Dennis <dkoluris@outlook.com>
2021-08-27 23:48:26 +0300

    Minor

--- src/GameServer/World.js
+++ src/GameServer/World.js
@@ -9,25 +9,28 @@ class World {
 
     static insertNpcs(session) {
         for (let i = 0; i < 20; i++) { // 20 Mobs
-            const coords = Utils.createRandomCoordinates(41819.5, 41705.1, 1200);
-            const number = Utils.createRandomNumber(this.npcDatabase.length);
-            const hp = 10 + Utils.createRandomNumber(85);
+            const coords  = Utils.createRandomCoordinates(41819.5, 41705.1, 1200);
+            const hp      = Utils.createRandomNumber(85) + 10;
             const heading = Utils.createRandomNumber(65536);
 
+            let npc = this.npcDatabase[
+                Utils.createRandomNumber(this.npcDatabase.length)
+            ];
+
             this.npcs.push({
-                id: 1000000 + i,
-                npcId: this.npcDatabase[number].id,
-                name: this.npcDatabase[number].name,
-                type:  this.npcDatabase[number].class,
+                        id: 1000000 + i,
+                     npcId: npc.id,
+                      name: npc.name,
+                      type: npc.class,
                 attackable: true,
-                hp: hp,
-                maxHp: 95,
-                collisionRadius: this.npcDatabase[number].collision_radius,
-                collisionHeight: this.npcDatabase[number].collision_height,
-                x: coords.x,
-                y: coords.y,
-                z: -3492,
-                heading: heading
+                        hp: hp,
+                     maxHp: 95,
+                    radius: npc.collision_radius,
+                      size: npc.collision_height,
+                         x: coords.x,
+                         y: coords.y,
+                         z: -3492,
+                   heading: heading
             });
 
             session.sendData(
