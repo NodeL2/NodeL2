370bb13e6cf88c88b3c6660cf32bd6ad28ef5703
Dennis Koluris <denniskoluris@rezolve.com>
2022-04-21 20:22:42 +0300

    More concise

--- src/AuthServer/Blowfish.js
+++ src/AuthServer/Blowfish.js
@@ -5,21 +5,17 @@ const blowfishKey = invoke('Config').optnAuthServer.blowfishKey + '\u0000';
 exports.blowfishDecrypt = (data) => {
     let decipher = createDecipheriv('bf-ecb', blowfishKey, '');
     decipher.setAutoPadding(false);
+    data = Buffer.concat([
+        decipher.update(data.swap32()), decipher.final()
+    ]);
 
-    data.swap32();
-    data = Buffer.concat([decipher.update(data), decipher.final()]);
-    data.swap32();
-
-    return data;
+    return data.swap32();
 };
 
 exports.blowfishEncrypt = (data) => {
     let cipher = createCipheriv('bf-ecb', blowfishKey, '');
     cipher.setAutoPadding(false);
+    data = cipher.update(data.swap32());
 
-    data.swap32();
-    data = cipher.update(data);
-    data.swap32();
-
-    return data;
+    return data.swap32();
 };
