af7357ed06359e53875e5219bb8d4031ebb9d251
Dennis <dkoluris@outlook.com>
2022-04-24 14:38:10 +0300

    Auth

--- src/AuthServer/Response/InitLS.js
+++ src/AuthServer/Response/InitLS.js
@@ -1,12 +1,16 @@
 let ServerPacket = invoke('ServerPacket');
+let RSA = invoke('RSA');
+
+let modulus = RSA.fetchModulus();
+let scrambledModulus = RSA.scrambleModulus(modulus);
 
 function initLS(serverProtocol) {
     let packet = new ServerPacket(0x00);
 
     packet
-        .writeD(0x00228afd)         // Session ID
-        .writeD(serverProtocol)     // Protocol
-        .writeB(Buffer.alloc(128)); // RSA Public Key
+        .writeD(0x00228afd)        // Session ID
+        .writeD(serverProtocol)    // Protocol
+        .writeB(scrambledModulus); // RSA Public Key
 
     return packet.fetchBuffer(false);
 }
--- src/RSA.js
+++ src/RSA.js
@@ -9,5 +9,29 @@ const RSA = {
 
     fetchModulus: () => {
         return key.exportKey('components-public').n;
+    },
+
+    scrambleModulus: (modulus) => {
+        let i, n = Buffer.from(modulus);
+
+        for (i = 0; i < 4; i++) {
+            [n[i], n[0x4d + i]] = [n[0x4d + i], n[i]];
+        }
+
+        for (i = 0; i < 0x40; i++) { n[0x00 + i] = n[0x00 + i] ^ n[0x40 + i]; }
+        for (i = 0; i < 0x04; i++) { n[0x0d + i] = n[0x0d + i] ^ n[0x34 + i]; }
+        for (i = 0; i < 0x40; i++) { n[0x40 + i] = n[0x40 + i] ^ n[0x00 + i]; }
+
+        return n;
+    },
+
+    decrypt: (encryptedText) => {
+        return key.decrypt(encryptedText, 'utf8');
+    },
+
+    encrypt: (text) => {
+        return key.encrypt(text, 'base64');
     }
 };
+
+module.exports = RSA;
