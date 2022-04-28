9c9ec773322a142ce13b76929d2b609dac24d8bb
Dennis Koluris <denniskoluris@rezolve.com>
2022-04-28 19:44:09 +0300

    Globals

---
+++ src/NodeL2.js
@@ -0,0 +1,23 @@
+require('./Globals');
+
+// User imports
+
+let AuthSession = invoke('AuthServer/Session');
+let Config      = invoke('Config');
+let Database    = invoke('Database');
+let Server      = invoke('Server');
+let Utils       = invoke('Utils');
+
+console.log('# ================================');
+console.log('# Server Name: ...... NodeL2 [768]');
+console.log('# Build Revision: ... %s', Utils.buildNumber());
+console.log('# Chronicle: ........ C4');
+console.log('# Build date: ....... %s', Utils.currentDate());
+console.log('# NodeJS version: ... %s', Utils.nodeVersion());
+console.log('# ================================\n');
+
+// Startup procedure, first `Database`, then `AuthServer`, finally `GameServer`
+
+Database.init(() => {
+    new Server('AuthServer', Config.authServer, (socket) => { new AuthSession(socket); });
+});
--- src/Server.js
+++ src/Server.js
@@ -1,25 +1,3 @@
-global.invoke = (module) => {
-    return require(__dirname + '/' + module);
-};
-
-global.fatalError = (...args) => {
-    console.log.apply(this, args);
-    process.exit();
-};
-
-let AuthSession = invoke('AuthServer/Session');
-let Utils       = invoke('Utils');
-
-console.log('# ================================');
-console.log('# Server Name: ...... NodeL2 [768]');
-console.log('# Build Revision: ... %s', Utils.buildNumber());
-console.log('# Chronicle: ........ C4');
-console.log('# Build date: ....... %s', Utils.currentDate());
-console.log('# NodeJS version: ... %s', Utils.nodeVersion());
-console.log('# ================================\n');
-
-// Generic class `Server` used by both AuthServer & GameServer
-
 class Server {
     constructor(name, optn, callback) {
         let parameters = { name: name, callback: callback };
@@ -40,20 +18,4 @@ class Server {
     }
 }
 
-// Startup procedure, first `Database`, then `AuthServer`, finally `GameServer`
-
-invoke('Database').init(() => {
-    new Server('AuthServer', invoke('Config').authServer, (socket) => { new AuthSession(socket); });
-});
-
-// Chronicle 0/1/2
-// [0x5b,0x3b,0x27,0x2e,0x5d,0x39,0x34,0x2d,0x33,0x31,0x3d,0x3d,0x2d,0x25,0x26,0x40,0x21,0x5e,0x2b,0x5d]
-// [;'.]94-31==-%&@!^+]\000
-
-// Chronicle 4/5
-// [0x5f,0x3b,0x35,0x2e,0x5d,0x39,0x34,0x2d,0x33,0x31,0x3d,0x3d,0x2d,0x25,0x78,0x54,0x21,0x5e,0x5b,0x24]
-// _;5.]94-31==-%xT!^[$\000
-
-// Chronicle 6 (Interlude)
-// [0x6b,0x60,0xcb,0x5b,0x82,0xce,0x90,0xb1,0xcc,0x2b,0x6c,0x55,0x6c,0x6c,0x6c,0x6c]
-// k`Ë[‚Î±Ì+lUllllk\000
+module.exports = Server;
