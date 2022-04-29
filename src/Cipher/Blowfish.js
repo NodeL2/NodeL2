f92e0191e8db4e737cece0c998f17bbbc5a99351
Dennis Koluris <denniskoluris@rezolve.com>
2022-04-29 15:10:35 +0300

    Some comments

--- src/Cipher/Blowfish.js
+++ src/Cipher/Blowfish.js
@@ -1,9 +1,10 @@
 let { createDecipheriv, createCipheriv } = require('crypto');
 
-const key = invoke('Config').authServer.blowfishKey + '\u0000';
+// Before L2 C6 interlude, the Blowfish secret was hardcoded in the clients. We know it beforehand.
+let key = invoke('Config').authServer.blowfishKey + '\u0000';
 
 const Blowfish = {
-    decrypt: function(data) {
+    decrypt: function(data) { // Switch endianness to decode
         let decipher = createDecipheriv('bf-ecb', key, '').setAutoPadding(false);
 
         data = Buffer.concat([
@@ -13,7 +14,7 @@ const Blowfish = {
         return data.swap32();
     },
 
-    encrypt: function(data) {
+    encrypt: function(data) { // Switch endianness to encode
         let cipher = createCipheriv('bf-ecb', key, '').setAutoPadding(false);
         data = cipher.update(data.swap32());
 
--- src/Cipher/RSA.js
+++ src/Cipher/RSA.js
@@ -1,3 +1,4 @@
+// Standard encoding of `1024` bits, and `65537` exponent
 const keyPair = require('node-forge').pki.rsa.generateKeyPair({
     bits: 1024, e: 0x10001
 });
@@ -5,8 +6,9 @@ const keyPair = require('node-forge').pki.rsa.generateKeyPair({
 const RSA = {
     scrambleModulus: () => {
         let modulus = Buffer.from(keyPair.publicKey.n.toByteArray());
-        let i, scrambled = modulus.slice(1);
+        let i, scrambled = modulus.slice(1); // Skip the `0x00` at the start
 
+        // A standard method as to how the L2 Clients decode the RSA Modulus on their end
         for (i = 0; i < 4; i++) {
             [scrambled[i], scrambled[0x4d + i]] = [scrambled[0x4d + i], scrambled[i]];
         }
@@ -18,7 +20,7 @@ const RSA = {
         return scrambled;
     },
 
-    decrypt: (data) => {
+    decrypt: (data) => { // L2 Clients **do not** use `SPKI`, `PKCS1` or `PKCS8`
         return keyPair.privateKey.decrypt(data, 'RAW');
     }
 };
