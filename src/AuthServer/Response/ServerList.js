800884f80c253c4b11729d5ded70e0867aa80301
Dennis Koluris <denniskoluris@rezolve.com>
2022-04-30 15:22:42 +0300

    Game

--- src/AuthServer/Response/ServerList.js
+++ src/AuthServer/Response/ServerList.js
@@ -3,24 +3,26 @@ let ServerPacket = invoke('Packet/Server');
 function serverList(config, ipAddress) {
     let packet = new ServerPacket(0x04);
 
-    let host       = ipAddress.split('.');
+    let hostname   = ipAddress.split('.');
     let port       = config.port;
     let maxPlayers = config.maxPlayers
 
     packet
-        .writeC(1)          // Number of servers
-        .writeC(0)          // LS Number
-        .writeC(1)          // Server ID
-        .writeC(host[0])    // Server IP
-        .writeC(host[1])    // Server IP
-        .writeC(host[2])    // Server IP
-        .writeC(host[3])    // Server IP
-        .writeD(port)       // Server port
-        .writeC(100)        // Age limit
-        .writeC(0)          // PVP ? 1 = Yes, 0 = No
-        .writeH(0)          // Current players
-        .writeH(maxPlayers) // Max players
-        .writeC(1);         // Status ? 1 = Up, 0 = Down
+        .writeC(0x01)        // Amount of Servers
+        .writeC(0x01)        // Last Server ID
+        .writeC(0x01)        // Server ID
+        .writeC(hostname[0]) // Server IP
+        .writeC(hostname[1]) // Server IP
+        .writeC(hostname[2]) // Server IP
+        .writeC(hostname[3]) // Server IP
+        .writeD(port)        // Server port
+        .writeC(0x00)        // Age limit
+        .writeC(0x00)        // PVP ? Yes = 1, No = 0
+        .writeH(0x00)        // Connected players
+        .writeH(maxPlayers)  // Max players
+        .writeC(0x01)        // Status ? Up = 1, Down = 0
+        .writeD(0x00)        // Test Server & Clock ???
+        .writeC(0x00);       // Server Brackets ???
 
     return packet.fetchBuffer();
 }
