cf4601402880f83def7c05b35bc2161645e4bbf9
Dennis <dkoluris@outlook.com>
2022-05-09 21:37:54 +0300

    Hardo

--- src/Server/Game/Request/AuthLogin.js
+++ src/Server/Game/Request/AuthLogin.js
@@ -10,24 +10,33 @@ function authLogin(session, buffer) {
     packet
         .readS()  // Username
         .readD()  // Session Key (last)
-        .readD(); // Session Key (first)
+        .readD()  // Session Key (first)
+        .readD()
+        .readD();
 
     consume(session, {
         username    : packet.data[0],
         sessionKey1 : packet.data[2],
         sessionKey2 : packet.data[1],
+        loginKey1   : packet.data[3],
+        loginKey2   : packet.data[4],
     });
 }
 
 function consume(session, data) {
+    console.log('0x%s', Utils.toHex(data.username, 8)); // OK
+    console.log('0x%s', Utils.toHex(data.sessionKey1, 8));
+    console.log('0x%s', Utils.toHex(data.sessionKey2, 8));
+    console.log('0x%s', Utils.toHex(data.loginKey1, 8)); // OK
+    console.log('0x%s', Utils.toHex(data.loginKey2, 8));
     //if (Utils.matchSessionKeys(Config.client, data)) {
         //session.accountId = data.username;
 
-        Database.fetchCharacters('q').then((rows) => {
-            session.dataSend(
-                ServerResponse.charSelectInfo(rows)
-            );
-        });
+        //Database.fetchCharacters('q').then((rows) => {
+        //    session.dataSend(
+        //        ServerResponse.charSelectInfo(rows)
+        //    );
+        //});
     //}
 }
 
