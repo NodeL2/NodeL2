d49cf2b7d266ac6cd90594a3d40a9def911a1149
Dennis Koluris <denniskoluris@rezolve.com>
2022-05-13 15:47:22 +0300

    OK

--- src/Server/Game/OpcodesEx.js
+++ src/Server/Game/OpcodesEx.js
@@ -4,8 +4,8 @@ const Utils = invoke('Utils');
 // Establishes an `OpcodeEx` table to handle client packets
 const OpcodesEx = {
     table: (() => {
-        const table = new Array(0xff).fill((_, decipheredPacket) => {
-            fatalError('GameServer:: unknown extended opcode 0x%s', Utils.toHex(decipheredPacket[0]));
+        const table = new Array(0xff).fill((_, packet) => {
+            fatalError('GameServer:: unknown extended opcode 0x%s', Utils.toHex(packet[0]));
         });
 
         return table;
