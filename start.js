21b51780f248d888ab0344fb287ed93b318f2255
Dennis <dkoluris@outlook.com>
2022-05-16 18:23:32 +0300

    Better

--- start.js
+++
@@ -1,6 +0,0 @@
-const version = Number((process.versions.node.split('.'))[0]);
-const script  = 'node ' + (version >= 17 ? '--openssl-legacy-provider' : '') + ' src/NodeL2';
-
-require('child_process').exec(script, (error, stdout) => {
-    console.info(stdout);
-});
