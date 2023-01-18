a062ae0938bd73fe19e5de35855e63e119c140b5
Dennis Koluris <dkoluris@outlook.com>
2023-01-18 23:12:56 +0200

    Fix

--- src/Server/Game/Response/NpcInfo.js
+++ src/Server/Game/Response/NpcInfo.js
@@ -4,14 +4,6 @@ const Utils        = invoke('Utils');
 function npcInfo(npc) {
     const packet = new ServerPacket(0x0c);
 
-    console.info(npc.id);
-    console.info(npc.title);
-    console.info(npc.name);
-    console.info(npc.loc.x);
-    console.info(npc.loc.y);
-    console.info(npc.loc.z);
-    console.info(npc.loc.heading);
-
     packet
         .writeD(268449299)
         .writeC(0x01)  // Spawn Animation 0: Teleported 1: Default 2: Summoned
@@ -28,46 +20,44 @@ function npcInfo(npc) {
         .writeD(npc.loc.x)
         .writeD(npc.loc.y)
         .writeD(npc.loc.z)
-        .writeD(npc.loc.heading);
-        //.writeD(0x00)  // ?
-        //.writeD(0x00)  // pAtkSpd
-        //.writeD(0x00)  // mAtkSpd
-        //.writeE(0.0)   // runSpdMul
-        //.writeE(0.0)   // atkSpdMul
-        //.writeD(0x00)  // R. Hand
-        //.writeD(0x00)  // Armor Id
-        //.writeD(0x00)  // L. Hand
-        //.writeC(0x01)  // Alive
-        //.writeC(0x00)  // Running
-        //.writeC(0x00)  // In Water
-        //.writeC(0x00)  // Team Ordinal
-        //.writeD(0x00)  // Enchant Effect
-        //.writeD(0x00)  // Flying
-        //.writeD(0x00)  // ?
-        //.writeD(0x00)  // Color Effect
-        //.writeD(0x00)  // State
-        //.writeD(0x00)  // Transform Id
-        //.writeD(0x00)  // HP
-        //.writeD(0x00)  // MP
-        //.writeD(0x00)  // Max HP
-        //.writeD(0x00)  // Max MP
-        //.writeC(0x00)  // 2: Animation on spawn
-        //.writeD(0x00)
-        //.writeD(0x00)
-        //.writeT(npc.name)
-        //.writeD(0x00)  // NPC Name Id
-        //.writeD(0x00)  // NPC Title Id
-        //.writeC(0x00)  // PVP Flag
-        //.writeD(0x00)  // Reputation/Name Color
-        //.writeD(0x00)  // Clan Id
-        //.writeD(0x00)  // Clan Crest Id
-        //.writeD(0x00)  // Large Clan Crest Id
-        //.writeD(0x00)  // Ally Id
-        //.writeD(0x00)  // Ally Crest Id
-        //.writeC(0x00)  // Status Mask
-        //.writeH(0x00); // Abnormal Effects Size
-
-        console.info(packet.fetchBuffer());
+        .writeD(npc.loc.heading)
+        .writeD(0x00)  // ?
+        .writeD(0x00)  // pAtkSpd
+        .writeD(0x00)  // mAtkSpd
+        .writeE(0.0)   // runSpdMul
+        .writeE(0.0)   // atkSpdMul
+        .writeD(0x00)  // R. Hand
+        .writeD(0x00)  // Armor Id
+        .writeD(0x00)  // L. Hand
+        .writeC(0x01)  // Alive
+        .writeC(0x00)  // Running
+        .writeC(0x00)  // In Water
+        .writeC(0x00)  // Team Ordinal
+        .writeD(0x00)  // Enchant Effect
+        .writeD(0x00)  // Flying
+        .writeD(0x00)  // ?
+        .writeD(0x00)  // Color Effect
+        .writeD(0x00)  // State
+        .writeD(0x00)  // Transform Id
+        .writeD(0x00)  // HP
+        .writeD(0x00)  // MP
+        .writeD(0x00)  // Max HP
+        .writeD(0x00)  // Max MP
+        .writeC(0x00)  // 2: Animation on spawn
+        .writeD(0x00)
+        .writeD(0x00)
+        .writeT(npc.name)
+        .writeD(0x00)  // NPC Name Id
+        .writeD(0x00)  // NPC Title Id
+        .writeC(0x00)  // PVP Flag
+        .writeD(0x00)  // Reputation/Name Color
+        .writeD(0x00)  // Clan Id
+        .writeD(0x00)  // Clan Crest Id
+        .writeD(0x00)  // Large Clan Crest Id
+        .writeD(0x00)  // Ally Id
+        .writeD(0x00)  // Ally Crest Id
+        .writeC(0x00)  // Status Mask
+        .writeH(0x00); // Abnormal Effects Size
 
     return packet.fetchBuffer();
 }
