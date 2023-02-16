f2d77cd1f2671b7b47516cf14913416f0a612a08
Dennis Koluris <dkoluris@outlook.com>
2023-02-16 21:45:29 +0200

    Simplified

--- src/Server/Game/Network/Common.js
+++ src/Server/Game/Network/Common.js
@@ -3,18 +3,22 @@ const Database       = invoke('Server/Database');
 
 const Common = {
     fetchCharacters(session) {
+        function createPaperdoll(nextChar) {
+            return new Promise((done) => {
+                Database.fetchItems(nextChar.id).then((items) => {
+                    nextChar.paperdoll = new Array(15).fill({ "id": 0, "itemId": 0 });
+                    for (const item of items.filter(ob => ob.equipped === 1)) {
+                        nextChar.paperdoll[item.slot] = { "id": item.id, "itemId": item.itemId };
+                    }
+                    done();
+                });
+            });
+        }
+
         Database.fetchCharacters(session.accountId).then((userChars) => {
             userChars.reduce((previous, nextChar) => {
                 return previous.then(() => {
-                    return new Promise((done) => {
-                        Database.fetchItems(nextChar.id).then((items) => {
-                            nextChar.paperdoll = new Array(15).fill({ "id": 0, "itemId": 0 });
-                            for (const item of items.filter(ob => ob.equipped === 1)) {
-                                nextChar.paperdoll[item.slot] = { "id": item.id, "itemId": item.itemId };
-                            }
-                            done();
-                        });
-                    });
+                    return createPaperdoll(nextChar);
                 });
             }, Promise.resolve()).then(() => {
                 session.dataSend(
