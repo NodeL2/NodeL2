a938233ba28039d9a3bbf0c1c6fe8c269a7fcf74
Dennis Koluris <dkoluris@outlook.com>
2023-02-24 13:26:03 +0200

    Prerequisite

--- src/Server/Game/DataCache.js
+++ src/Server/Game/DataCache.js
@@ -7,15 +7,20 @@ const DataCache = {
 
         DataCache.classTemplates = validateModel(path + 'Templates/templates');
         DataCache.itemsNewbie    = validateModel(path + 'Templates/Gear/newbie');
+        DataCache.skillTree      = validateModel(path + 'Skills/Tree/tree');
         DataCache.npcs           = validateModel(path + 'Npcs/1-7400');
         DataCache.npcSpawns      = validateModel(path + 'Npcs/Spawns/spawns');
-        DataCache.skills         = validateModel(path + 'Skills/skills');
-        DataCache.skillTree      = validateModel(path + 'Skills/Tree/tree');
 
         DataCache.items = [
             ...validateModel(path + 'Items/Armors/armors'),
             ...validateModel(path + 'Items/Weapons/weapons'),
             ...validateModel(path + 'Items/Others/others')
+        ];
+
+        DataCache.skills = [
+            ...validateModel(path + 'Skills/Active/active'),
+            ...validateModel(path + 'Skills/Passive/passive'),
+            ...validateModel(path + 'Skills/Switch/switch')
         ];
     },
 
