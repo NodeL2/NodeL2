df0db0b6b7cc9643c1a1baf15863c05a16cfb06f
Otavio <otavio-bernardes@outlook.com>
2023-03-06 21:15:02 -0300

    chore: add docker-compose

--- database/sql/database.sql
+++ database/sql/database.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS `nodel2`;
-CREATE DATABASE `nodel2` COLLATE utf8_unicode_ci;
-use `nodel2`
+CREATE DATABASE IF NOT EXISTS nodel2  COLLATE utf8_unicode_ci;
+
+use nodel2;
 
 CREATE TABLE `accounts` (
     `username`    varchar(16) NOT NULL,
