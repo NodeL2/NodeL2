32556b187754bba9e3247cf5b1f9a2fc2c8c41bd
Dennis Koluris <dkoluris@outlook.com>
2023-03-21 21:16:34 +0200

    C4

--- src/NodeL2.js
+++ src/NodeL2.js
@@ -12,7 +12,7 @@ console.info('\n\
     + ================================== \n\
     # Server Name: ......... NodeL2      \n\
     # Build Revision: ...... %s          \n\
-    # Chronicle: ........... C2 [485]    \n\
+    # Chronicle: ........... C4 [485]    \n\
     # Build date: .......... %s          \n\
     # NodeJS version: ...... %s          \n\
     + ================================== \n\
