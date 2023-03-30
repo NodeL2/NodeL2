404b25c18ccca83a4001e62bc7d0a0cda04bad37
Dennis Koluris <dkoluris@outlook.com>
2023-03-30 16:56:30 +0300

    WIP

--- src/NodeL2.js
+++ src/NodeL2.js
@@ -8,26 +8,49 @@ const DataCache   = invoke('GameServer/DataCache');
 const Database    = invoke('Database');
 const Server      = invoke('Server');
 
-console.info('\n\
-    + ================================== \n\
-    # Server Name: ......... NodeL2      \n\
-    # Build Revision: ...... %s          \n\
-    # Chronicle: ........... C2 [485]    \n\
-    # Build date: .......... %s          \n\
-    # NodeJS version: ...... %s          \n\
-    + ================================== \n\
-', utils.buildNumber(), utils.currentDate(), utils.nodeVersion());
+//console.info('\n\
+//    + ================================== \n\
+//    # Server Name: ......... NodeL2      \n\
+//    # Build Revision: ...... %s          \n\
+//    # Chronicle: ........... C2 [485]    \n\
+//    # Build date: .......... %s          \n\
+//    # NodeJS version: ...... %s          \n\
+//    + ================================== \n\
+//', utils.buildNumber(), utils.currentDate(), utils.nodeVersion());
+//
+//// Startup procedure, init `World` & `Data`, then `AuthServer`, finally `GameServer`
+//Database.init(() => {
+//    DataCache.init();
+//        World.init();
+//
+//    new Server('AuthServer', options.default.AuthServer, (socket) => {
+//        return new AuthSession(socket);
+//    });
+//
+//    new Server('GameServer', options.default.GameServer, (socket) => {
+//        return new GameSession(socket);
+//    });
+//});
 
-// Startup procedure, init `World` & `Data`, then `AuthServer`, finally `GameServer`
-Database.init(() => {
-    DataCache.init();
-        World.init();
+let spawns = require('./spawns');
+let names = require('./names');
 
-    new Server('AuthServer', options.default.AuthServer, (socket) => {
-        return new AuthSession(socket);
-    });
+for (let spawn of spawns) {
+    const inside = spawn.spawns;
+    if (!inside) {
+        console.log('0');
+    }
+    else {
+        for (let one of inside) {
+            const found = names.find(ob => ob.selfId === one.selfId);
+            if (!found) {
+                console.log(one.selfId);
+            }
+            else {
+                one.name = found.Name;
+            }
+        }
+    }
+}
 
-    new Server('GameServer', options.default.GameServer, (socket) => {
-        return new GameSession(socket);
-    });
-});
+//require('fs').writeFile('output.json', JSON.stringify(names), 'utf8', () => {});
