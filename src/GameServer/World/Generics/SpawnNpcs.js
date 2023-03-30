4e34358c4164032b95aa177330fdb54944d9c67c
Dennis Koluris <dkoluris@outlook.com>
2023-03-30 15:49:06 +0300

    WIP

--- src/GameServer/World/Generics/SpawnNpcs.js
+++ src/GameServer/World/Generics/SpawnNpcs.js
@@ -2,12 +2,33 @@ const Npc       = invoke('GameServer/Npc/Npc');
 const DataCache = invoke('GameServer/DataCache');
 
 function spawnNpcs() {
-    DataCache.npcs.forEach((npc) => {
-        const spawns = DataCache.npcSpawns.filter(ob => ob.selfId === npc.selfId)[0]?.spawns ?? [];
-        spawns.forEach((coords) => {
-            this.npc.spawns.push(
-                new Npc(this.npc.nextId++, utils.crushOb({ ...npc, ...coords }))
-            );
+//    DataCache.npcs.forEach((npc) => {
+//        const spawns = DataCache.npcSpawns.filter(ob => ob.selfId === npc.selfId)[0]?.spawns ?? [];
+//        spawns.forEach((coords) => {
+//            this.npc.spawns.push(
+//                new Npc(this.npc.nextId++, utils.crushOb({ ...npc, ...coords }))
+//            );
+//        });
+//    });
+//
+//    utils.infoSuccess('Spawns', '%d Npcs & Monsters', this.npc.spawns.length);
+
+    DataCache.npcSpawns.forEach((item) => {
+        const bounds = item.bounds;
+        const spawns = item.spawns;
+
+        spawns.forEach((spawn) => {
+            const npc = DataCache.npcs.find((ob) => ob.selfId === spawn.selfId);
+            if (npc && spawn.coords[0]) {
+                const coords = {
+                    locX: spawn.coords[0].locX,
+                    locY: spawn.coords[0].locY,
+                    locZ: spawn.coords[0].minZ,
+                }
+                this.npc.spawns.push(
+                    new Npc(this.npc.nextId++, utils.crushOb({ ...npc, ...coords }))
+                );
+            }
         });
     });
 
