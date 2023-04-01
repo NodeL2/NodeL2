2e7f752e5697c00c085b1d56e64e4705b5114f03
Dennis Koluris <dkoluris@outlook.com>
2023-04-01 15:53:51 +0300

    Corpse time

--- src/GameServer/Actor/Generics/StopAutomation.js
+++ src/GameServer/Actor/Generics/StopAutomation.js
@@ -1,12 +1,16 @@
+const ServerResponse = invoke('GameServer/Network/Response');
+
 function stopAutomation(session, actor) {
     actor.automation.abortAll(actor);
 
-    invoke(path.actor).updatePosition(session, actor, {
-        locX: actor.fetchLocX(),
-        locY: actor.fetchLocY(),
-        locZ: actor.fetchLocZ(),
-        head: actor.fetchHead(),
-    });
+    session.dataSend(
+        ServerResponse.stopMove(actor.fetchId(), {
+            locX: actor.fetchLocX(),
+            locY: actor.fetchLocY(),
+            locZ: actor.fetchLocZ(),
+            head: actor.fetchHead(),
+        })
+    );
 }
 
 module.exports = stopAutomation;
