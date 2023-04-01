f81c52d0e406a4adced4255fe3848c837522665b
Dennis Koluris <dkoluris@outlook.com>
2023-04-01 22:14:27 +0300

    Revert on ticks

--- src/GameServer/Automation.js
+++ src/GameServer/Automation.js
@@ -80,7 +80,7 @@ class Automation extends SelectedModel {
     }
 
     ticksToMove(srcX, srcY, srcZ, dstX, dstY, dstZ, radius, speed) {
-        const distance = Formulas.calcDistance3D(srcX, srcY, srcZ, dstX, dstY, dstZ) - radius;
+        const distance = Formulas.calcDistance(srcX, srcY, dstX, dstY) - radius;
         const duration = 1 + ((this.ticksPerSecond * distance) / speed);
         return (1000 / this.ticksPerSecond) * duration;
     }
