a49c5f8ccbe825c446351078ec3a313cad222ed7
Dennis Koluris <dkoluris@outlook.com>
2023-04-05 16:57:39 +0300

    First movement broadcast

--- src/GameServer/Actor/Generics/MoveTo.js
+++ src/GameServer/Actor/Generics/MoveTo.js
@@ -1,4 +1,6 @@
 const ServerResponse = invoke('GameServer/Network/Response');
+const World          = invoke('GameServer/World/World');
+const Formulas       = invoke('GameServer/Formulas');
 
 function moveTo(session, actor, coords) {
     if (actor.isDead()) {
@@ -13,6 +15,13 @@ function moveTo(session, actor, coords) {
     // Abort scheduled movement, user redirected the actor
     actor.automation.abortAll(actor);
     session.dataSend(ServerResponse.moveToLocation(actor.fetchId(), coords));
+
+    const sessions = World.user.sessions.filter((ob) => Formulas.calcWithinRadius(coords.from.locX, coords.from.locY, ob.actor?.fetchLocX(), ob.actor?.fetchLocY(), 5000)) ?? [];
+    sessions.forEach((user) => {
+        if (user !== session) {
+            user.dataSend(ServerResponse.moveToLocation(actor.fetchId(), coords));
+        }
+    });
 }
 
 module.exports = moveTo;
